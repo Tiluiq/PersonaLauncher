1a956063c23dc363c43ffc2560283220a7dea1d5
BuildTools <unconfigured@null.spigotmc.org>
2020-05-24 19:32:59 +0900

    ver 0.0.1 背景透過 Canvas上でのアニメーション

---
+++ PersonaLauncher/PersonaLauncher/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonaLauncher
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			//どこかを持てばドラッグして移動できるようにする
+			//this.MouseLeftButtonDown += (sender, e) => { this.DragMove(); };
+		}
+
+		public void Data_Click(object sender, MouseButtonEventArgs e)
+		{
+			//MessageBox.Show("データがクリックされました");
+
+		}
+
+		//ペルソナをドラッグできるようにする
+		private void Persona_Drag(object sender, MouseButtonEventArgs e)
+		{
+			this.DragMove();
+		}
+	}
+}
